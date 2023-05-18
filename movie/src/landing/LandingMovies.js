406f11acb7409cf9150f4845b0b4e9760d12e1a6
Ethan-Kettle <133995847+Ethan-Kettle@users.noreply.github.com>
2023-05-18 10:50:06 -0600

    Update LandingMovies.js

--- movie/src/landing/LandingMovies.js
+++ movie/src/landing/LandingMovies.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import '../scss/landingMovies.scss'
-const key = '6b9f844777c1c7871dcdef89004ab36e';
+const key = 'f839cc5221a4442baa47bf5494ebe08b';
 
 export default function LandingMovies({getLandingMovies}) {
   async function ApiCall() {
